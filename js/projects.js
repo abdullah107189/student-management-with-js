5f9b3626f69d8ddbeab1d3c4f522bef0c167d5a1
abdullah107189 <abdullah107189@gmail.com>
2024-09-17 22:22:35 +0600

    added array to add exapmle p tag under

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,26 @@
+const studentsInfoArray = [];
 const addStudentBtn = document.getElementById('addStudentBtn')
-addStudentBtn.addEventListener('click', (e) =>{
+addStudentBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    window.location.href='student-info.html'
+    const inputName = document.getElementById('input-name')
+    const inputMarks = document.getElementById('input-marks')
+    if (inputName.value === '' || inputMarks.value === '') {
+        return alert('please full fill input')
+    }
+    const studentInfoObject = {
+        'name': inputName.value,
+        'marks': inputMarks.value
+    }
+    studentsInfoArray.push(studentInfoObject)
+
+
+    const infoHere = document.getElementById('infoHere')
+    infoHere.innerHTML = ''
+    for (let studentInfo of studentsInfoArray) {
+        infoHere.innerHTML += ` <p>name: ${studentInfo.name} marks: ${studentInfo.marks} </p>`
+    }
+
+    inputName.value = ''
+    inputMarks.value = ''
+    // window.location.href = 'student-info.html'
 })
